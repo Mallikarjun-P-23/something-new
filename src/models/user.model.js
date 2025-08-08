5d7aa7d535f0c2f956f5c666fc976820e83a40ff
Mallikarjun-P-23 <mallikarjunparoji23@gmail.com>
2025-08-08 13:07:34 +0530

    initial

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose,{ Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
 
 const userSchema =new Schema({
      username:{
@@ -44,7 +47,43 @@ const userSchema =new Schema({
      }
 },{
     timestamps:true
-}
+  }
 )
+
+userSchema.pre('save',async function(next) {
+   if(!this.isModified("password")) return next();
+   this.password = bcrypt.hash(this.password,10)
+   next();
+})
+
+userSchema.method.isPasswordCorrect =async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.method.generateToken = function(){
+    return jwt.sign(
+      {
+         _id:this._id,
+         email:this.email,
+         fullname:this.fullname,
+         username:this.username
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+         expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '1d'
+      }
+    )
+}
+userSchema.method.generateRefreshToken = function() {
+   return jwt.sign(
+      {
+         _id:this._id
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+         expiresIn: process.env.REFRESH_TOKEN_EXPIRY || '10d'
+      }
+    )
+}
 
 export const User = mongoose.model('User',userSchema);
